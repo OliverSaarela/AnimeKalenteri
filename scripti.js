15143cb2ee85690f9cc36578d29ee32a6343e454
OliverSaarela <43965623+OliverSaarela@users.noreply.github.com>
2018-12-01 01:44:04 +0200

    Update scripti.js

--- scripti.js
+++ scripti.js
@@ -3,7 +3,7 @@ var paiva = thisDay.getDate();
 var kuukausi = thisDay.getMonth();
 
 function kuvanVaihto(luukku) {
-    if (luukku <= paiva && kuukausi == 10) {
+    if (luukku <= paiva && kuukausi == 11) {
         document.getElementById("day" + luukku + "img").src = "images/img" + luukku + ".png";
     }
 }
