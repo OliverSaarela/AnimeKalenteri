dce25d1b70f1b6e66dd960f93ef0e9f855f8ad47
Lawn738 <44837018+Lawn738@users.noreply.github.com>
2018-12-04 22:51:11 +0200

    Add files via upload

--- scripti.js
+++ scripti.js
@@ -2,8 +2,24 @@ var thisDay = new Date();
 var paiva = thisDay.getDate();
 var kuukausi = thisDay.getMonth();
 
+
 function kuvanVaihto(luukku) {
     if (luukku <= paiva && kuukausi == 11) {
-        document.getElementById("day" + luukku + "img").src = "images/img" + luukku + ".png";
+        document.getElementById("day" + luukku + "img").src = "images/img" + luukku + ".png" ;
+		document.getElementById("day" + luukku + "img").setAttribute("onclick","avaa()");
+        document.getElementById("valittu").src = "images/img" + luukku + ".png" ;
     }
+    
 }
+
+
+function nollaa() { 
+    document.getElementById("showcase").style.display = "none";
+    document.getElementById("valittu").src = "" ;
+}
+
+function avaa() {
+    document.getElementById("showcase").style.display = "block";
+    document.getElementById("valittu").src = "images/img" + luukku + ".png" ;
+}
+
